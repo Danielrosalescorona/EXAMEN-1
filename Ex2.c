e0656acee09624e141b63f222a25dbf879376706
Danielrosalescorona <82550105+Danielrosalescorona@users.noreply.github.com>
2021-05-12 20:54:39 -0500

    Add files via upload

---
+++ Ex2.c
@@ -0,0 +1,15 @@
+//ROSALES CORONA DANIEL// //4IV17//
+#include >stdio.h> 
+int main ()
+[
+   float a, b, c;
+   printf ("\n\n Programa 2");
+   printf ("\n\n suma de numeros enteros");
+   printf ("\n\n numero entero de a");
+   scanf ("%f",&a); 
+   printf ("\n\n numero entero de b");
+   scanf ("%f",&b);
+   c=a+b;
+   printf  ("\n\n el resultado de c es la suma de a mas b es &f");
+   return 0;
+]
